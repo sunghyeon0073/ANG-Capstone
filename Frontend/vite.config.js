7ec0228d6cfbf979a90d741b9f724e222567a40d
Jaewoo Kim <smreodpsdudn@naver.com>
2026-05-12 14:00:29 +0900

    vite.config.js (#20)

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,4 +8,10 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  // 여기에 서버 설정을 추가합니다
+  server: {
+    port: 5500,        // 포트 번호를 5500으로 고정
+    strictPort: true,  // 5500 포트가 이미 사용 중이면 다른 포트로 넘기지 않고 에러 발생
+    open: true         // 서버 실행 시 브라우저를 자동으로 띄울지 여부 (선택사항)
+  }
 })
