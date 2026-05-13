2e0212526a1d0165191ef7825445af74f794ae09
YJU <aya21110@naver.com>
2026-05-13 19:18:14 +0900

    🌐 Api 연결 다시다시 수정
    api 연결 다시다시 수정

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://15.168.156.224/:9090', // 백엔드 서버 주소
+  baseURL: 'http://15.168.156.224:9090', // 백엔드 서버 주소
   timeout: 5000, // 5초 이상 응답 없으면 에러
   headers: {
     'Content-Type': 'application/json',
