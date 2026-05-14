e0c0015b763bf273bc912a668ded7e6f4ad451a3
jaewoo kim <smreodpsdudn@naver.com>
2026-05-14 19:16:29 +0900

    DocumentWriter fix

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -9,13 +9,17 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem('token')
 
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`
   }
 
-  return config;
-});
+  if (!(config.data instanceof FormData)) {
+    config.headers['Content-Type'] = 'application/json'
+  }
+
+  return config
+})
 
 export default api;
