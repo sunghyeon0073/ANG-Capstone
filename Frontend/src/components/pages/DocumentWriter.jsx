542c24d73646c319b77717b697a0d9772bba500c
YJU <aya21110@naver.com>
2026-06-09 21:35:54 +0900

    🐛 + 업로드 버튼 삭제
    + 업로드 버튼 삭제

--- Frontend/src/components/pages/DocumentWriter.jsx
+++ Frontend/src/components/pages/DocumentWriter.jsx
@@ -634,16 +634,6 @@ export default function DocumentWriter() {
             >
               <FiPlus />
             </button>
-            <button
-              type="button"
-              className="document-upload-icon-btn"
-              onClick={() => setShowUploadModal(true)}
-              disabled={isUploading}
-              title="파일 업로드"
-              aria-label="파일 업로드"
-            >
-              <FiPlus />
-            </button>
           </div>
 
           <div className="document-content">
